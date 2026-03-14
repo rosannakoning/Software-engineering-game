f22de96eb3dd935510a24855ccb47fd6ed128f6d
Bodiel Siebers <bodielsiebers@macbookpro-1.home>
2026-03-14 17:21:17 +0100

    Basic pygame window works

--- game.py
+++ game.py
@@ -1,1 +1,18 @@
-print("Hello Software Engineering Game")
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("Software Engineering Game")
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill((0,0,0))
+    pygame.display.update()
+
+pygame.quit()
