c1958113c5d3ec408613fc7c25dc67b49afa210c
Hajar Hosaini <hajarhosaini56@icloud.com>
2026-03-17 14:59:49 +0100

    Geluidsbestanden toegevoegd, muziek aangezet en achtergrond blauw gemaakt

--- game.py
+++ game.py
@@ -3,7 +3,8 @@ from objects import spawn_object
 
 pygame.init()
 
-## sounds 
+## --- SOUNDS ---
+# Let op: Zorg dat deze bestanden echt in de map 'sounds' staan!
 catch_sound = pygame.mixer.Sound("sounds/fallingsounds.wav")
 catch_sound.set_volume(0.5)
 
@@ -17,7 +18,7 @@ background_song = pygame.mixer.Sound("sounds/backgroundsong.wav")
 background_song.set_volume(0.05)
 
 
-
+# Scherm instellingen
 screen_width = 800
 screen_height = 600
 screen = pygame.display.set_mode((screen_width, screen_height))
@@ -33,9 +34,12 @@ falling_objects = []
 spawn_timer = 0
 spawn_delay = 40
 
-# HIER GING HET MIS: Score moet op 0 beginnen
+# Score instellingen
 score = 0 
 font = pygame.font.SysFont(None, 36)
+
+# --- START DE MUZIEK HIER ---
+background_song.play(-1)  # -1 betekent dat het liedje oneindig herhaalt
 
 running = True
 while running:
@@ -69,23 +73,26 @@ while running:
 
         # Check voor botsing met mandje
         if obj.get_rect().colliderect(basket):
-            score += 1  # Score gaat omhoog als je iets vangt!
+            score += 1 
+            catch_sound.play()  # Speel geluid als je iets vangt!
             falling_objects.remove(obj)
+        
         # Verwijder als het object de grond raakt
         elif obj.is_off_screen(screen_height):
+            missing_sound.play() # Speel geluid als je mist!
             falling_objects.remove(obj)
 
-    # TEKENEN
-    screen.fill((0, 0, 0)) # Zwarte achtergrond
+    # --- TEKENEN ---
+    screen.fill((135, 206, 235)) # Blauwe lucht
 
-    # Teken mandje (nu bruin voor de herkenbaarheid)
+    # Teken mandje
     pygame.draw.rect(screen, (139, 69, 19), basket)
 
     # Teken de vallende objecten
     for obj in falling_objects:
         obj.draw(screen)
 
-    # Score op het scherm zetten
+    # Score op het scherm
     score_text = font.render(f"Score: {score}", True, (255, 255, 255))
     screen.blit(score_text, (20, 20))
 
