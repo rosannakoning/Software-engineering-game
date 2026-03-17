3a8ce5d06bcb80e523402495a1deb47763e6ef87
Hajar Hosaini <hajarhosaini56@icloud.com>
2026-03-17 14:29:55 +0100

    Mijn wijzigingen opslaan voor de merge met Bo

--- game.py
+++ game.py
@@ -3,21 +3,6 @@ from objects import spawn_object
 
 pygame.init()
 
-## sounds 
-catch_sound = pygame.mixer.Sound("/Users/romynguyen/Software-engineering-game/sounds/fallingsounds.wav")
-catch_sound.set_volume(0.5)
-
-missing_sound = pygame.mixer.Sound("/Users/romynguyen/Software-engineering-game/sounds/error.wav")
-missing_sound.set_volume(0.8)
-
-start_sound = pygame.mixer.Sound("/Users/romynguyen/Software-engineering-game/sounds/button.wav")
-start_sound.set_volume(0.5)
-
-background_song = pygame.mixer.Sound("/Users/romynguyen/Software-engineering-game/sounds/backgroundsong.wav")
-background_song.set_volume(0.05)
-
-
-
 screen_width = 800
 screen_height = 600
 screen = pygame.display.set_mode((screen_width, screen_height))
@@ -34,48 +19,18 @@ spawn_delay = 40
 
 font = pygame.font.SysFont(None, 36)
 
-def start_screen(screen, font):
-    waiting = True
-
-    while waiting:
-        screen.fill((0, 0, 0))
-
-        title_text = font.render("Fruit Game", True, (255, 255, 255))
-        start_text = font.render("Press SPACE to Start", True, (200, 200, 200))
-
-        screen.blit(title_text, (300, 250))
-        screen.blit(start_text, (250, 300))
-
-        pygame.display.flip()
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                exit()
-
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE:
-                    start_sound.play()
-                    waiting = False
-
-
-start_screen(screen, font)
-
 running = True
 while running:
     clock.tick(60)
-    background_song.play()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    # --- BESTURING AANGEPAST VOOR PIJLTJES ---
+    # DE BESTURING (A/D EN PIJLTJES)
     keys = pygame.key.get_pressed()
-    # Nu werkt A én het linkerpijltje
     if keys[pygame.K_a] or keys[pygame.K_LEFT]:
         basket.x -= basket_speed
-    # Nu werkt D én het rechterpijltje
     if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
         basket.x += basket_speed
 
@@ -94,15 +49,13 @@ while running:
         obj.update()
 
         if obj.get_rect().colliderect(basket):
-            catch_sound.play()
             falling_objects.remove(obj)
         elif obj.is_off_screen(screen_height):
-            missing_sound.play()
             falling_objects.remove(obj)
 
     screen.fill((0, 0, 0))
 
-    # mandje tekenen
+    # Mandje tekenen
     pygame.draw.rect(screen, (139, 69, 19), basket)
     pygame.draw.line(screen, (160, 82, 45), (basket.x, basket.y + 10), (basket.x + basket.width, basket.y + 10), 2)
     pygame.draw.line(screen, (160, 82, 45), (basket.x, basket.y + 20), (basket.x + basket.width, basket.y + 20), 2)
